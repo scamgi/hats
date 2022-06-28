d5ce9de3d947746c9b942023a4f0aa00ae9c0a79
scamgi <scampini.giacomo@gmail.com>
2022-06-28 19:58:40 +0200

    div.hat added

--- src/views/RandomHatView.vue
+++ src/views/RandomHatView.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+import Hat from "../components/Hat.vue";
+import BlackHatIcon from "../components/icons/BlackHatIcon.vue";
+</script>
+
 <template>
-random hat
+  <div class="hat">
+    <Hat>
+      <template #icon>
+        <BlackHatIcon/>
+      </template>
+      <template #hat-name>
+        Cappello nero
+      </template>
+      ecc
+    </Hat>
+  </div>
 </template>
+
+<style scoped>
+.hat {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+  width: 100%;
+}
+</style>
