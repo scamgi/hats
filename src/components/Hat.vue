8aced54de1d55b5021b5acb96d5e4f5fffb2a669
scamgi <scampini.giacomo@gmail.com>
2022-06-28 19:56:19 +0200

    hat component added

---
+++ src/components/Hat.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="item">
+    <slot name="icon"></slot>
+    <div class="details">
+      <h5>
+        <slot name="hat-name"></slot>
+      </h5>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
