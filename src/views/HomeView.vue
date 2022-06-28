20663f8da019da48d3eeabea51b00fe046316632
scamgi <scampini.giacomo@gmail.com>
2022-06-28 19:45:20 +0200

    links added to homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,13 +10,32 @@ import TheWelcome from "@/components/TheWelcome.vue";
  -->
 
 <template>
-  <!-- <div class="temp"> -->
-    home
-  <!-- </div> -->
+  <div class="welcome">
+    <h1>Welcome!</h1>
+    <div class="row">
+      <div class="three columns">
+        <router-link to="/" class="button">Home</router-link>
+      </div>
+      <div class="three columns">
+        <router-link to="/random-hat" class="button">Random Hat</router-link>
+      </div>
+      <div class="three columns">
+        <a href="#" class="button">esempio</a>
+      </div>
+      <div class="three columns">
+        <a href="#" class="button">esempio</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-/* .temp {
-  min-height: 100vh;
-} */
+.welcome {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+  width: 100%;
+}
 </style>
