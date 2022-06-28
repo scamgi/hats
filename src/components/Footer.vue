9bad6f8c0970742c1070a8c77372e9b4f35bc7c1
scamgi <scampini.giacomo@gmail.com>
2022-06-28 19:29:45 +0200

    home link and random hat link added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,10 +6,16 @@ import IconGithub from './icons/IconGithub.vue';
   <div class="footer">
     <div class="container">
       <div class="row">
-        <div class="two columns">esempio</div>
-        <div class="three columns">esempio</div>
         <div class="two columns">
-          <IconGithub></IconGithub>
+          <router-link to="/">Home</router-link>
+        </div>
+        <div class="three columns">
+          <router-link to="/random-hat">Random Hat</router-link>
+        </div>
+        <div class="two columns">
+          <a href="https://github.com/scamgi/hats">
+            <IconGithub></IconGithub>
+          </a>
         </div>
         <div class="three columns">esempio</div>
         <div class="two columns">esempio</div>
@@ -21,8 +27,8 @@ import IconGithub from './icons/IconGithub.vue';
 <style scoped>
 
 .footer {
-  padding: 50px;
-  border-top: 5px solid #222;
+  padding: 70px 15px;
+  border-top: 2px solid #ddd;
 }
 
 .row>div {
