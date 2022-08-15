81ec21d7cbab3b37d896be92b4d9e7953a4100a0
Giacomo Scampini <scampini.giacomo@gmail.com>
2022-08-16 00:22:45 +0200

    howitworks view modified

--- src/views/HowItWorksView.vue
+++ src/views/HowItWorksView.vue
@@ -1,24 +1,25 @@
 <script lang="ts">
 import hats from '@/assets/hats';
+import Hat from '../components/Hat.vue';
 
 export default {
-  name: 'HowItWorks',
-  data() {
-    return {
-      hats: null
-    }
-  },
-  created() {
-    this.hats = hats;
-  }
+    name: "HowItWorks",
+    data() {
+        return {
+            hats: null
+        };
+    },
+    created() {
+        this.hats = hats;
+    },
+    components: { Hat }
 }
 </script>
 
 <template>
-  <div v-for="hat in hats">
-    <h5>{{hat.name}} - {{hat.description}}</h5>
+  <Hat v-for="hat in hats" :hatName="hat.name + ' - ' + hat.description" :hatColor="hat.color">
     <ul>
       <li v-for="quote in hat.quotes">{{quote}}</li>
     </ul>
-  </div>
+  </Hat>
 </template>
