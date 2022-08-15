675a6b5227a75b6ae37143db29ef960b6aff569c
scamgi <scampini.giacomo@gmail.com>
2022-08-15 23:04:08 +0200

    HowItWorksView.vue added

---
+++ src/views/HowItWorksView.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import hats from '@/assets/hats';
+
+export default {
+  name: 'HowItWorks',
+  data() {
+    return {
+      hats: null
+    }
+  },
+  created() {
+    this.hats = hats;
+  }
+}
+</script>
+
+<template>
+  <div>{{hats[0].name}}</div>
+</template>
