de845b7e36daf5095633a564ed98b3b77e692327
Giacomo Scampini <scampini.giacomo@gmail.com>
2022-08-16 00:37:01 +0200

    this.session modified

--- src/views/MakeSessionView.vue
+++ src/views/MakeSessionView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import SessionPlanner from "../components/SessionPlanner.vue";
 import SessionDisplay from "../components/SessionDisplay.vue";
+import hats from "@/assets/hats";
 </script>
 
 <script lang="ts">
@@ -12,38 +13,39 @@ export default {
     }
   },
   created() {
-    this.session = [
-      {
-        color: "#4361ee",
-        name: "Blue hat",
-        minutes: 10
-      },
-      {
-        color: "#354f52",
-        name: "Green hat",
-        minutes: 15
-      },
-      {
-        color: "#ef233c",
-        name: "Red hat",
-        minutes: 15
-      },
-      {
-        color: '#000',
-        name: "Black hat",
-        minutes: 60
-      },
-      {
-        color: '#fff',
-        name: "White hat",
-        minutes: 45
-      },
-      {
-        color: '#ffb703',
-        name: 'Yellow hat',
-        minutes: 30
-      }
-    ]
+    // this.session = [
+    //   {
+    //     color: "#4361ee",
+    //     name: "Blue hat",
+    //     minutes: 10
+    //   },
+    //   {
+    //     color: "#354f52",
+    //     name: "Green hat",
+    //     minutes: 15
+    //   },
+    //   {
+    //     color: "#ef233c",
+    //     name: "Red hat",
+    //     minutes: 15
+    //   },
+    //   {
+    //     color: '#000',
+    //     name: "Black hat",
+    //     minutes: 60
+    //   },
+    //   {
+    //     color: '#fff',
+    //     name: "White hat",
+    //     minutes: 45
+    //   },
+    //   {
+    //     color: '#ffb703',
+    //     name: 'Yellow hat',
+    //     minutes: 30
+    //   }
+    // ]
+    this.session = hats;
   }
 }
 </script>
