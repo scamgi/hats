0531a3f3a4f5d1c44d6c187462f05701a0512d6a
Giacomo Scampini <scampini.giacomo@gmail.com>
2022-08-20 01:35:36 +0200

    indentation modified

--- src/views/HowItWorksView.vue
+++ src/views/HowItWorksView.vue
@@ -3,23 +3,23 @@ import hats from '@/assets/hatsList';
 import Hat from '../components/Hat.vue';
 
 export default {
-    name: "HowItWorks",
-    data() {
-        return {
-            hats: null
-        };
-    },
-    created() {
-        this.hats = hats;
-    },
-    components: { Hat }
+  name: "HowItWorks",
+  data() {
+    return {
+      hats: null
+    };
+  },
+  created() {
+    this.hats = hats;
+  },
+  components: { Hat }
 }
 </script>
 
 <template>
   <Hat v-for="hat in hats" :hatName="hat.name + ' - ' + hat.description" :hatColor="hat.color">
     <ul>
-      <li v-for="quote in hat.quotes">{{quote}}</li>
+      <li v-for="quote in hat.quotes">{{ quote }}</li>
     </ul>
   </Hat>
 </template>
