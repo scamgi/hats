4621a341c18bbe8ef76597c360d7fcdeda020140
Giacomo Scampini <scampini.giacomo@gmail.com>
2022-08-30 22:50:08 +0200

    changes in the homescreen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,28 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import tree from '@/assets/tree-home-page.jpeg';
 
 export default defineComponent({
   name: 'HomeView',
+  data() {
+    return {
+      tree
+    }
+  }
 });
 </script>
 
 <template>
   <v-container>
-    <h1>This is the 6 thinking hats app!</h1>
-    This is an app to brainstorm new ideas using the 6 thinking hats developed by Edward de Bono. To get started, select the mode you want to use.
+    <v-row align="center">
+      <v-col md="6" cols="12">
+        <v-img :src="tree" contain max-height="600" />
+      </v-col>
+      <v-col md="6" cols="12">
+        <div class="text-h1">6 thinking hats model</div>
+        This is an app to brainstorm new ideas using the 6 thinking hats developed by Edward de Bono. To get started, select the mode you want to use.
+      </v-col>
+    </v-row>
   </v-container>
-  <v-row justify="center" class="pa-10">
-  </v-row>
 </template>
 
