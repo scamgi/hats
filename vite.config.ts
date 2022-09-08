8cafa60e0d9c6073c768f974f9649fb0135628a4
scamgi <scampini.giacomo@gmail.com>
2022-09-09 00:12:02 +0200

    todo added

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+// TODO: set the correct base! (see https://vitejs.dev/guide/static-deploy.html#github-pages)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
